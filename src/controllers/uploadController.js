6a11bf3bdefbeb2ce148633e058273dbced07515
VIBHOR <183467237+vibhor-777@users.noreply.github.com>
2026-06-14 13:19:32 +0530

    Add uploadIdCard function for ID card uploads

---
+++ src/controllers/uploadController.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { sendSuccess, sendError, asyncHandler } = require('../utils/responseHelper');
+const User = require('../models/User');
+
+const uploadIdCard = asyncHandler(async (req, res) => {
+  if (!req.file) {
+    return sendError(res, 'No file uploaded', 400);
+  }
+
+  const fileUrl = `/uploads/id-cards/${req.file.filename}`;
+
+  await User.findByIdAndUpdate(req.user.id, {
+    idCardUrl: fileUrl,
+    verificationStatus: 'pending'
+  });
+
+  return sendSuccess(
+    res,
+    { idCardUrl: fileUrl, verificationStatus: 'pending' },
+    'ID card uploaded. Your account will be verified within 24 hours.'
+  );
+});
+
+module.exports = { uploadIdCard };
