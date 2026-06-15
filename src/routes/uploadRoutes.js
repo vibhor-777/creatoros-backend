1c2922a355caa94dcff59a33f2e14fc0a2e573a7
VIBHOR <183467237+vibhor-777@users.noreply.github.com>
2026-06-15 10:23:48 +0530

    Update uploadRoutes.js

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const { auth } = require('../middleware/auth');
 const { uploadImage } = require('../middleware/uploadMiddleware');
 const { uploadIdCard } = require('../controllers/uploadController');
 
